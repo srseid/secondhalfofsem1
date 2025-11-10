d67ad7e5674bc4b1210467892be2f21e4022277f
srseid <148483902+srseid@users.noreply.github.com>
2025-11-10 15:59:40 -0500

    add week10 package

---
+++ Assets/Scripts/PlayerVisuals.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerVisuals : MonoBehaviour
+{
+    public Animator animator;
+    public SpriteRenderer bodyRenderer;
+    public PlayerController playerController;
+
+    private readonly int isWalkingHash = Animator.StringToHash("IsWalking");
+    private readonly int isGroundedHash = Animator.StringToHash("IsGrounded");
+
+    void Update()
+    {
+        animator.SetBool(isWalkingHash, playerController.IsWalking());
+        animator.SetBool(isGroundedHash, playerController.IsGrounded());
+
+        switch (playerController.GetFacingDirection())
+        {
+            case PlayerController.FacingDirection.left:
+                bodyRenderer.flipX = true;
+                break;
+            case PlayerController.FacingDirection.right:
+                bodyRenderer.flipX = false;
+                break;
+        }
+    }
+}
