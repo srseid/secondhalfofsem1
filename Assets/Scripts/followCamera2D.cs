45f6bdc8e61d49ac9b831a71540c8e83974543aa
srseid <148483902+srseid@users.noreply.github.com>
2025-12-01 13:52:48 -0500

    make camera follow player

---
+++ Assets/Scripts/followCamera2D.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[RequireComponent(typeof(Camera))]
+public class followCamera2D : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private Tilemap tilemap;
+
+    private Camera followCamera;
+    private Vector3 offset;
+    private Vector2 viewportHalfSize;
+
+    private float leftCameraBound;
+    private float rightCameraBound;
+    private float bottomCameraBound;
+
+    void Start()
+    {
+        followCamera = GetComponent<Camera>();
+        offset = transform.position - target.position;
+
+        CalculateCameraBounds();
+    }
+    private void CalculateCameraBounds()
+    { 
+        tilemap.CompressBounds();
+
+        float orthoSize = followCamera.orthographicSize; 
+        viewportHalfSize = new (orthoSize * followCamera.aspect, orthoSize);
+
+        Vector3Int tilemapMin = tilemap.cellBounds.min;
+        Vector3Int tilemapMax = tilemap.cellBounds.max;
+
+        leftCameraBound = tilemapMin.x + viewportHalfSize.x;
+        rightCameraBound = tilemapMax.x - viewportHalfSize.x;
+        bottomCameraBound = tilemapMin.y + viewportHalfSize.y;
+        //set bounds
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 steppedPosition = Vector3.Lerp(transform.position, desiredPosition, speed * Time.deltaTime);
+
+        steppedPosition.x = Mathf.Clamp(steppedPosition.x, leftCameraBound, rightCameraBound);
+        steppedPosition.y = Mathf.Clamp(steppedPosition.y, bottomCameraBound, steppedPosition.y);
+        //make sure camera cannot see out of bounds
+        transform.position = steppedPosition;
+    }
+
+}
